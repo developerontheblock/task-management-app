871efbfa39f5a2435b54a4057681aba1e5dff4f7
Alex Draev <alexanderdraev@gmail.com>
2021-11-06 10:58:14 +0200

    export endpoints logic to controllers

--- backend/routes/tasks-routes.js
+++ backend/routes/tasks-routes.js
@@ -1,44 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const HttpError = require('../models/http-error');
 
-const DUMMY_TASKS = [
-    {
-        id: 't1',
-        title: 'bug task',
-        description: 'This task contains a bug',
-        creator: 'u1'
-    }
-];
+const tasksContollers = require('../contollers/tasks-controllers');
 
-router.get('/:tid', (req, res, next) => {
-    const taskId = req.params.tid;
-    const task = DUMMY_TASKS.find(t => {
-        return t.id === taskId;
-    });
+router.get('/:tid',  tasksContollers.getTaskById);
 
-    if (!task) {
-        throw new HttpError('Could not find a task for the provided id.', 404);
-    }
-
-    res.json({task});
-});
-
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const task = DUMMY_TASKS.find(t => {
-        return t.creator === userId;
-    });
-
-    if (!task) {
-        return next(
-            new HttpError('Could not find a task for the provided user id.', 404)
-        );
-    }
-
-    res.json({task});
-});
+router.get('/user/:uid', tasksContollers.getTaskByUserId);
 
 module.exports = router;
 
